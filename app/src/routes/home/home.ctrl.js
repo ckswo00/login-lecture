3146dd2a1505ce6d6c8710bfedc90f30b32883be
ckswo00 <ckswo00@naver.com>
2023-11-19 09:35:41 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,12 +1,41 @@
-const hello = (req, res) => {
-    res.render("home/index");
+"use strict";
+
+const output = {
+    home : (req, res) => {
+        res.render("home/index");
+    },
+    login : (req, res) =>{
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) =>{
-    res.render("home/login");
+const users ={
+    id: ["chanjae", "poir", "이찬재"],
+    psword: ["1234", "1234", "123456"],
+};
+
+const process = {
+    login : (req, res) => {
+        const id = req.body.id,
+              psword = req.body.psword;
+        
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    seucess: true,
+                });
+            }
+        }
+
+        return res.json({
+            sucess: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
 };
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 };
